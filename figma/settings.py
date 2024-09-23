8d0be28dee75958c85b1e5391803ae02de562284
sandra <sandravarghese1010@gmail.com>
2024-09-23 21:44:41 +0530

    fourth commit

--- figma/settings.py
+++ figma/settings.py
@@ -86,6 +86,13 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
