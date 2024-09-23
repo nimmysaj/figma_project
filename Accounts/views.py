46be9b04103f0c44be33055378f0e7322d4e8836
NJ <76942502+NJ-games@users.noreply.github.com>
2024-09-23 17:01:26 +0530

    login with username and password

--- Accounts/views.py
+++ Accounts/views.py
@@ -1,3 +1,28 @@
+from rest_framework.views import APIView
 from django.shortcuts import render
+from .models import User
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from rest_framework.response import Response
 
 # Create your views here.
+
+class LoginView(APIView):
+    def post(self,request):
+        try:
+            user=User.objects.get(username=request.data.get('username'),password=request.data.get('password'))
+            refresh=RefreshToken.for_user(user)
+            access=refresh.access_token
+            return Response({
+            'message':"login successful",
+            'refresh':str(refresh),
+            'access':str(access)
+            
+            },status=status.HTTP_200_OK)
+        except user.DoesNotExist :
+            return Response({'message':"invalid"},status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
--- figma/settings.py
+++ figma/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'Accounts',
     'customer',
     'service_provider',
+    'rest_framework',
+    'rest_framework_simplejwt'
 ]
 
 MIDDLEWARE = [
@@ -81,6 +83,12 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
 }
 
 
