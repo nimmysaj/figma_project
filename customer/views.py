b4b44337f57624fb3a9fe0aecbf25a9f72315be8
NJ <76942502+NJ-games@users.noreply.github.com>
2024-09-25 21:16:48 +0530

    more robust forgot password workflow

--- customer/views.py
+++ customer/views.py
@@ -1,5 +1,7 @@
 from rest_framework.views import APIView
 from django.shortcuts import render
+from django.core import signing
+from django.contrib.auth.tokens import default_token_generator
 from rest_framework.permissions import IsAuthenticated,AllowAny
 from rest_framework import status,generics
 from rest_framework.response import Response
@@ -51,38 +53,33 @@ class VerifyOTPView(generics.GenericAPIView):
             otp = OTP.objects.get(otp_code=otp_code)
             if otp.is_expired():
                 return Response({"error": "OTP has expired."}, status=status.HTTP_400_BAD_REQUEST)
-            reset_token = RefreshToken.for_user(otp.user)
-            reset_token['scope'] = 'password_reset'
             return Response({
                 'message': 'OTP verified successfully',
-                'resetToken': str(reset_token.access_token) 
+                'resetToken': default_token_generator.make_token(otp.user),
+                'userId' : signing.dumps(otp.user.id)
             }, status=status.HTTP_200_OK)
         except OTP.DoesNotExist:
             return Response({"error": "Invalid OTP."}, status=status.HTTP_400_BAD_REQUEST)
 
 class NewPasswordView(generics.UpdateAPIView):
-    queryset = User.objects.all()
     serializer_class = NewPasswordSerializer
     permission_classes=[AllowAny]
     def post(self, request):
         reset_token = request.data.get('resetToken')
         new_password = request.data.get('newPassword')
+        user_id=request.data.get('userId')
+        user=User.objects.get(id=signing.loads(user_id))
+        is_valid = default_token_generator.check_token(user, reset_token)
 
-        # Verify the JWT token and check the scope
-        try:
-            decoded_token = JWTAuthentication().get_validated_token(reset_token)
 
-            # Check if the token's scope is 'password_reset'
-            if decoded_token.get('scope') != 'password_reset':
-                return Response({'error': 'Invalid token scope.'}, status=status.HTTP_403_FORBIDDEN)
+        if is_valid:
+            serial=self.serializer_class(user,data={'password':new_password})
+            if serial.is_valid() :
+                serial.save()
+                return Response({'message': 'Password changed successfully'}, status=status.HTTP_200_OK)
+            else:
+                return Response(serial.errors, status=status.HTTP_400_BAD_REQUEST)
 
-            # Token is valid, reset the user's password
-            user = User.objects.get(id=decoded_token['user_id'])
-            user.password=new_password
-            user.save()
-
-            return Response({'message': 'Password changed successfully'}, status=status.HTTP_200_OK)
-
-        except InvalidToken:
+        else:
             return Response({'error': 'Invalid or expired token.'}, status=status.HTTP_400_BAD_REQUEST)
 
