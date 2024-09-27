f3df8b99c6161fec6884f685ec1c7096472875e6
Sharath Kumar K <sharathkumar.k00@gmail.com>
2024-09-27 12:22:42 +0530

    Removed duplicate addition of `REST_FRAMEWORK` variable
    Removed duplicate addition of `REST_FRAMEWORK` variable

--- figma/settings.py
+++ figma/settings.py
@@ -90,15 +90,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
-}
-
-
-
-
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
