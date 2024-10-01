19fd75de6402f7a0cb1ac8cb44cb13e31f97149f
nimmysaj <160224590+nimmysaj@users.noreply.github.com>
2024-10-01 12:52:10 +0530

    Update admin.py

--- Accounts/admin.py
+++ Accounts/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User,Country_Codes
+from .models import OTP, Customer, Dealer, District, Franchise_Type, Franchisee, ServiceProvider, State
+
 # Register your models here.
 
 class UserAdmin(BaseUserAdmin):
@@ -21,3 +23,12 @@ class UserAdmin(BaseUserAdmin):
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Country_Codes)
+admin.site.register(Customer)
+admin.site.register(ServiceProvider)
+admin.site.register(Franchisee)
+admin.site.register(Franchise_Type)
+admin.site.register(Dealer)
+admin.site.register(OTP)
+admin.site.register(District)
+admin.site.register(State)
+
