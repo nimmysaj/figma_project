4b73f59936f61419a5e238c09ce630d5a4f58a88
Anjana <anjanavsann1995@gmail.com>
2024-10-07 20:34:30 +0530

    patch updated

--- admin_app/serializers.py
+++ admin_app/serializers.py
@@ -1,9 +1,13 @@
 from rest_framework import serializers
 from Accounts.models import User
+from django.contrib.auth.hashers import make_password
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        # fields = ['email','full_name','nationality','designation','phone_number','is_active','is_staff']
-        fields = '__all__'
+        exclude = ['password']
+        # fields = ['id','last_login','is_customer','is_service_provider','is_franchisee','is_dealer','is_active','email','full_name','nationality','designation','phone_number','is_active','is_staff']
+        # fields = '__all__'
+
+        
         
