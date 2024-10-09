7c511962f4af0a4efc33f330ba8ca8f486b4c4ac
Sanitha <sanitha044@gmail.com>
2024-10-09 10:11:25 -0700

    Complaint of serviceprovider done

--- service_provider/admin.py
+++ service_provider/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from Accounts.models import User,Dealer,Franchisee,Franchise_Type,ServiceProvider, District, State, Complaint, ServiceRequest
+from Accounts.models import User,Dealer,Franchisee,Franchise_Type,ServiceProvider, District, State, Complaint, ServiceRequest, ServiceRegister,Category,Subcategory,Service_Type,Collar
 
 # Register your models here.
 # admin.site.register(User)
@@ -11,4 +11,8 @@ admin.site.register(District)
 admin.site.register(State)
 admin.site.register(Complaint)
 admin.site.register(ServiceRequest)
-
+admin.site.register(ServiceRegister)
+admin.site.register(Category)
+admin.site.register(Subcategory)
+admin.site.register(Service_Type)
+admin.site.register(Collar)
