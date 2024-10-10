1f340e0d1d0799c18b76423b8ded526cb663b7a0
Liba Sherin M <libasherin.m7684@gmail.com>
2024-10-10 20:47:18 +0530

    pending_request

---
+++ adminapp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+from Accounts.models import ServiceRequest
+from Accounts.models import ServiceRegister
+from Accounts.models import Collar
+from Accounts.models import Category
+from Accounts.models import Subcategory
+from Accounts.models import Service_Type
