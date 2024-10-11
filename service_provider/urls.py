6b047405c181eb37c13b471d650c1fd6e7fd7c33
nimmysaj <160224590+nimmysaj@users.noreply.github.com>
2024-10-11 16:03:18 +0530

    Update urls.py

--- service_provider/urls.py
+++ service_provider/urls.py
@@ -1,5 +1,9 @@
-from django.urls import path
-from .views import ServiceProviderLoginView, ServiceProviderPasswordForgotView, ServiceProviderPasswordForgotView, ResetPasswordView, ServiceProviderViewSet, SetNewPasswordView
+from django.urls import include, path
+from .views import CustomerServiceRequestView, ServiceProviderLoginView, ServiceProviderPasswordForgotView, ServiceProviderPasswordForgotView, ResetPasswordView, ServiceProviderRequestsView, ServiceProviderViewSet, ServiceRegisterViewSet, ServiceRequestInvoiceView, SetNewPasswordView
+from rest_framework.routers import DefaultRouter
+router = DefaultRouter()
+router.register(r'service-registers', ServiceRegisterViewSet, basename='service-register')
+
 
 urlpatterns = [
     path('login/', ServiceProviderLoginView.as_view(), name='service-provider-login'),
@@ -12,5 +16,12 @@ urlpatterns = [
         'get': 'retrieve', 
         'put': 'update',
         'patch': 'partial_update'
-        }), name='profile_update')
+        }), name='profile_update'),
+    #service register,edit,lead balance
+    path('', include(router.urls)),
+    #service request
+    path('service-requests/', ServiceProviderRequestsView.as_view(), name='service-provider-requests'),
+    path('service-requests/details/<int:pk>/', CustomerServiceRequestView.as_view(), name="details"),
+    path('invoice/<int:pk>/', ServiceRequestInvoiceView.as_view(), name="invoice")
+    
 ]
