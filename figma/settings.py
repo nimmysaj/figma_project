9bef71d672e3654f24e0c47f3245cbadd39a3154
Ben Joy <benjoybj891@gmail.com>
2024-10-23 08:52:16 +0530

    work status updated

--- figma/settings.py
+++ figma/settings.py
@@ -157,24 +157,16 @@ SIMPLE_JWT = {
     'BLACKLIST_AFTER_ROTATION': False,
 }
 
-#email smtp
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'  # SMTP server host
-# EMAIL_PORT = 587  # SMTP port
-# EMAIL_USE_TLS = True  # Use TLS encryption
-# EMAIL_HOST_USER = ''  # Your email
-# EMAIL_HOST_PASSWORD = ''  # Your email password
-# DEFAULT_FROM_EMAIL = ''  # Default from email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'  # SMTP server host
+EMAIL_PORT = 587  # SMTP port
+EMAIL_USE_TLS = True  # Use TLS encryption
+EMAIL_HOST_USER = ''  # Your email
+EMAIL_HOST_PASSWORD = ''  # Your email password
+DEFAULT_FROM_EMAIL = ''  # Default from email
 
 # settings.py
 #TWILIO_ACCOUNT_SID = ''
 #TWILIO_AUTH_TOKEN = ''
 #TWILIO_PHONE_NUMBER = ''
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'localhost'  # smtp4dev runs locally
-EMAIL_PORT = 25  # Default port used by smtp4dev
-EMAIL_USE_TLS = False  # TLS is not needed for local testing
-EMAIL_HOST_USER = ''  # No authentication required for smtp4dev
-EMAIL_HOST_PASSWORD = ''  # No authentication required for smtp4dev
-DEFAULT_FROM_EMAIL = 'test@example.com'  # You can set any default email
