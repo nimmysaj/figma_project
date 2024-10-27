b9b8c9a8b9b56f270df75af8b00819247f334e83
Amjith <amjithsreekumar@gmail.com>
2024-10-27 21:59:13 +0530

    service provider list dashboard for franchise

--- figma/urls.py
+++ figma/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('customer/', include('customer.urls')),
     path('service-provider/', include('service_provider.urls')),
-    path('franchisee/', include('franchisee.urls'))
+    path('franchisee/', include('franchise.urls'))
 ]
 
 if settings.DEBUG:
--- franchise/apps.py
+++ franchise/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class FranschiseConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'franchisee'
+    name = 'franchise'
