9dcc426af02fea3d2be373d732d33623a141b706
vaisakh <vaisakhn02@gmail.com>
2024-10-28 20:01:40 +0530

    resolved merge conflicts

--- Dealer/urls.py
+++ Dealer/urls.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.urls import path
 from .views import ServiceProviderVerificationListCreate, ServiceProviderVerificationDetail,DealerLoginView
 
@@ -7,13 +6,4 @@ urlpatterns = [
     path('service-providers/<int:pk>/', ServiceProviderVerificationDetail.as_view(), name='service-provider-detail'),
     path('login/', DealerLoginView.as_view(), name='dealer_login'),
 
-=======
-from django.urls import include, path
-from .views import ServiceProviderView,ProviderSearchView,DealerLoginView
-
-urlpatterns = [
-    path('login/', DealerLoginView.as_view(), name='dealer_login'),
-    path('service-providers-details/', ServiceProviderView.as_view(), name='service-provider-details'),
-    path('service-providers-search/', ProviderSearchView.as_view(), name='service-provider-search'),
->>>>>>> f568851c93265555359d959eef8979965dcd140b
 ]
