0276cdbd9dc737bc23ba2f2616fd6b0be36177f7
Sharath Kumar K <sharathkumar.k00@gmail.com>
2024-10-28 10:16:37 +0530

    Remove review field from ServiceRequestDetails

--- customer/serializers.py
+++ customer/serializers.py
@@ -234,9 +234,8 @@ class ServiceRequestSerializer(serializers.ModelSerializer):
 
 class ServiceRequestDetailSerializer(serializers.ModelSerializer):
     invoices = InvoiceSerializer(many=True, read_only=True)
-    reviews = CustomerReviewSerializer(many=True, read_only=True)
     class Meta:
         model = ServiceRequest
-        fields = ['id', 'customer', 'service_provider', 'service', 'work_status', 'acceptance_status', 'availability_from', 'availability_to', 'additional_notes', 'invoices', 'image', 'reviews']
+        fields = ['id', 'customer', 'service_provider', 'service', 'work_status', 'acceptance_status', 'availability_from', 'availability_to', 'additional_notes', 'invoices', 'image']
 
 
