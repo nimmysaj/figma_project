e72ae7cef4fa01c6307477deae8849bd4b71b2b1
greeshmajopaul <greesh7@gmail.comgit config --global user.email greesh7@gmail.com>
2024-11-18 12:10:07 +0530

    Fix folder casing issue from dealer to Dealer

--- Dealer/urls.py
+++ Dealer/urls.py
@@ -1,23 +1,10 @@
-from django.urls import path
-<<<<<<< HEAD
-from .views import DealerPaymentRequestAPIView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ServiceProviderViewSet
+
+router = DefaultRouter()
+router.register(r'service-providers', ServiceProviderViewSet)
 
 urlpatterns = [
-    path('api/dealer/payment-request/', DealerPaymentRequestAPIView.as_view(), name='dealer-payment-request'),
+    path('', include(router.urls)),
 ]
-=======
-from .views import ServiceProviderVerificationListCreate, ServiceProviderVerificationDetail,DealerLoginView, DealerFranchiseeListView, DealerPaymentHistoryView
-from Dealer import views
-
-urlpatterns = [
-    path('service-providers/', ServiceProviderVerificationListCreate.as_view(), name='service-providers-list'),
-    path('service-providers/<int:pk>/', ServiceProviderVerificationDetail.as_view(), name='service-provider-detail'),
-    path('login/', DealerLoginView.as_view(), name='dealer_login'),
-    path('serviceprovider/verification/',views.ServiceProviderVerificationListView.as_view()),
-    path('dealer/franchisee/', DealerFranchiseeListView.as_view(), name='dealer-franchisee-list'),
-    path('dealer/payment-history/', DealerPaymentHistoryView.as_view(), name='dealer-payment-history'),
-]
-
-
-
->>>>>>> 71e1ae1a8e69921af81f6a16cac16c250fd80e25
--- dealer/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from Accounts.models import ServiceProvider
-
-class ServiceProviderVerificationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ServiceProvider
-        fields = '__all__'
-
